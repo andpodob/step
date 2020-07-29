4a4fe7310dcd8e05464e9fa499c03eed0a98a742
AndrzejPodo <andrzejpodobinski2@gmail.com>
2020-07-29 10:08:19 +0000

    finish basic authentication feature and nickname management

---
+++ portfolio/src/main/java/com/google/sps/servlets/NicknameServlet.java
@@ -0,0 +1,39 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/nickname")
+public class NicknameServlet extends HttpServlet {
+    @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+    if (!userService.isUserLoggedIn()) {
+      response.sendRedirect("/");
+      return;
+    }
+
+    String nickname = request.getParameter("nickname");
+    String id = userService.getCurrentUser().getUserId();
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Entity entity = new Entity("UserInfo", id);
+    entity.setProperty("id", id);
+    entity.setProperty("nickname", nickname);
+    // The put() function automatically inserts new data or updates existing data based on ID
+    datastore.put(entity);
+
+    response.sendRedirect("/");
+  }
+}
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -201,6 +201,17 @@ function prevComments(){
     });
 }
 
+/**
+ * 
+ * @param {*} authData json authentication data fetched from server
+ * 
+ * Based on server response it creates AuthData object that contains login state
+ * LoginState is evaluated based on response:
+ * -if user is not logged in LoginState if obviously LOGGED_OUT
+ * -if user is logged in and the nickname was not contained in response than 
+ *     it means user haven't set nickname yet hence the loginState is LOGGED_IN_NICKNAME_NOT_SET
+ * -if user is logged in and the nickname was send back from server than state is LOGGED_IN_NICKNAME_SET
+ */
 function handleAuthData(authData){
     let loginState = LoginState.LOGGED_OUT;
     const nickname = authData.nickname;
@@ -211,7 +222,7 @@ function handleAuthData(authData){
     if(typeof authData.isUserLoggedIn !== 'undefined' && authData.isUserLoggedIn == false){
         loginState = LoginState.LOGGED_OUT;
     }else{
-        if(typeof AuthData.nickname !== 'undefined'){
+        if(typeof authData.nickname !== 'undefined'){
             loginState = LoginState.LOGGED_IN_NICKNAME_SET;
         }else{
             loginState = LoginState.LOGGED_IN_NICKNAME_NOT_SET;
@@ -221,6 +232,45 @@ function handleAuthData(authData){
     return new AuthData(loginState, userId, nickname, loginUrl, logoutUrl);
 }
 
+/**
+ * navigates to loginUrl provided by authentication servlet 
+ */
+function login(loginUrl){
+    window.location.href = loginUrl;
+}
+
+/**
+ * navigates to logoutUrl provided by authentication servlet 
+ */
+function logout(logoutUrl){
+    window.location.href = logoutUrl;
+}
+
+/**
+ * hides and shows appropriate elements after clicking change nickname button 
+ */
+function changeNicknameButtonAction(){
+    const changeNicknameButton = document.getElementById('change-nickname-button');
+    const nicknameForm = document.getElementById('nickname-form');
+    const cancelNicknameChangeButton = document.getElementById('cancel-nickname-change-button');
+
+    changeNicknameButton.style.display = 'none';
+    nicknameForm.style.display = 'block';
+    cancelNicknameChangeButton.style.display = 'block';
+}
+
+/**
+ * hides and shows appropriate elements after clicking cancel button 
+ */
+function cancelNicknameChangeButtonAction(){
+    const changeNicknameButton = document.getElementById('change-nickname-button');
+    const nicknameForm = document.getElementById('nickname-form');
+    const cancelNicknameChangeButton = document.getElementById('cancel-nickname-change-button');
+
+    changeNicknameButton.style.display = 'block';
+    nicknameForm.style.display = 'none';
+    cancelNicknameChangeButton.style.display = 'none';
+}
 /**
  * Fetches auth data and then creates AuthData object that can be used to update frontend.
  * Function returns Promise to enable chaining it with functions that are using AuthData object
@@ -240,41 +290,57 @@ function fetchAuthData(){
 function manageAuthFrontEnd(authData){
     const loginMessage = document.getElementById('login-message');
     const loginButton = document.getElementById('login-button');
-    const setNicknameButton = document.getElementById('set-nickname-button');
+    const cancelNicknameChangeButton = document.getElementById('cancel-nickname-change-button');
     const changeNicknameButton = document.getElementById('change-nickname-button');
     const logoutButton = document.getElementById('logout-button');
     const commentForm = document.getElementById('comment-form');
+    const nicknameForm = document.getElementById('nickname-form');
+    const userName = document.getElementById('user-name');
     switch(authData.loginState){
         case LoginState.LOGGED_OUT:
             loginMessage.innerHTML = 'In order to leave a comment you need to authenticate.';
+            loginButton.onclick = (event) => {login(authData.loginUrl);}
             loginButton.style.display = 'block';
             logoutButton.style.display = 'none';
-            setNicknameButton.style.display = 'none';
             changeNicknameButton.style.display = 'none';
+            cancelNicknameChangeButton.style.display = 'none';
             commentForm.style.display = 'none';
+            nicknameForm .style.display = 'none';
             break;
         case LoginState.LOGGED_IN_NICKNAME_SET:
+            userName.value = authData.nickname;
             loginMessage.innerHTML = `Hi ${authData.nickname}`;
+            logoutButton.onclick = (event) => {login(authData.logoutUrl);}
             loginButton.style.display = 'none';
             logoutButton.style.display = 'block';
-            setNicknameButton.style.display = 'none';
             changeNicknameButton.style.display = 'block';
+            cancelNicknameChangeButton.style.display = 'none';
             commentForm.style.display = 'block';
+            nicknameForm.style.display = 'none';
             break;
         case LoginState.LOGGED_IN_NICKNAME_NOT_SET:
             loginMessage.innerHTML = `Please set Your nick name in order to leave a comment.`;
+            logoutButton.onclick = (event) => {login(authData.logoutUrl);}
             loginButton.style.display = 'none';
             logoutButton.style.display = 'block';
-            setNicknameButton.style.display = 'block';
             changeNicknameButton.style.display = 'none';
+            cancelNicknameChangeButton.style.display = 'none';
             commentForm.style.display = 'none';
+            nicknameForm .style.display = 'block';
             break;
     }
 }
 
+/**
+ * fetches auth data and updates authentication connected part of frontend
+ */
+function updateAuthView(){
+    fetchAuthData().then((authData) => this.manageAuthFrontEnd(authData));
+}
+
 window.onload = function(){
     getNewestComments();
-    fetchAuthData().then((authData) => this.manageAuthFrontEnd(authData));
+    this.updateAuthView();
     const popupBackground = document.getElementById("popup-background")
 
     const tiles = document.getElementsByClassName("tile");
