95ca34ef322497146fe24f5131f6645e5593209f
AndrzejPodo <andrzejpodobinski2@gmail.com>
2020-08-05 12:47:02 +0000

    add optional attendees feature

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -16,6 +16,7 @@ package com.google.sps;
 
 import java.util.Collection;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Stack;
 
 public final class FindMeetingQuery {
@@ -26,33 +27,68 @@ public final class FindMeetingQuery {
      * freeIntervals - used to collect intevals that are between busyIntervals   
      */
     ArrayList<TimeRange> importantRanges = new ArrayList<TimeRange>();
+    ArrayList<TimeRange> optionalImportantRanges = new ArrayList<TimeRange>();
     Stack<TimeRange> busyIntervals = new Stack<TimeRange>();
+    Stack<TimeRange> optionalBusyIntervals = new Stack<TimeRange>();
     ArrayList<TimeRange> freeIntervals = new ArrayList<TimeRange>();
+    ArrayList<TimeRange> freeIntervalsWithOptionals = new ArrayList<TimeRange>();
     
     for(String attendee : request.getAttendees()){
       for (Event event : events){
         if(event.getAttendees().contains(attendee)){
           importantRanges.add(event.getWhen());
+          optionalImportantRanges.add(event.getWhen());
         }
       }
     }
-
-    importantRanges.sort(TimeRange.ORDER_BY_START);
-   
-    TimeRange popedRange;
-    for(TimeRange timeRange : importantRanges){
-      if(busyIntervals.size() > 0 && timeRange.overlaps(busyIntervals.peek())){
-        popedRange = busyIntervals.pop();
-        busyIntervals.push(mergeTimeRanges(popedRange, timeRange));
-      }else{
-        busyIntervals.push(timeRange);
+    
+    for(String attendee : request.getOptionalAttendees()){
+      for (Event event : events){
+        if(event.getAttendees().contains(attendee)){
+          optionalImportantRanges.add(event.getWhen());
+        }
+      }
+    }
+    //if there are no occupied or optionaly occupied intervals to consider we can immediately return list with WHOLE_DAY
+    if(optionalImportantRanges.size() == 0){
+      if(request.getDuration() <= TimeRange.WHOLE_DAY.duration()){
+        return Arrays.asList(TimeRange.WHOLE_DAY);
+      } else{
+        return Arrays.asList();
       }
     }
 
+    optionalImportantRanges.sort(TimeRange.ORDER_BY_START);
+    importantRanges.sort(TimeRange.ORDER_BY_START);
+    
+    TimeRange popedRange;
+    if(importantRanges.size() > 0){
+      busyIntervals.push(importantRanges.get(0));
+      for(TimeRange timeRange : importantRanges){
+        if(timeRange.overlaps(busyIntervals.peek())){
+          popedRange = busyIntervals.pop();
+          busyIntervals.push(mergeTimeRanges(popedRange, timeRange));
+        }else{
+          busyIntervals.push(timeRange);
+        }
+      }
+    }
+  
+    optionalBusyIntervals.push(optionalImportantRanges.get(0));
+    for(TimeRange timeRange : optionalImportantRanges){
+      if(timeRange.overlaps(optionalBusyIntervals.peek())){
+        popedRange = optionalBusyIntervals.pop();
+        optionalBusyIntervals.push(mergeTimeRanges(popedRange, timeRange));
+      }else{
+        optionalBusyIntervals.push(timeRange);
+      }
+    }
     int duration;
     //dummy TimeRanges to provide limits to the entire space
     busyIntervals.add(0, new TimeRange(TimeRange.START_OF_DAY, TimeRange.START_OF_DAY));
     busyIntervals.add(new TimeRange(TimeRange.END_OF_DAY+1, TimeRange.END_OF_DAY+1));
+    optionalBusyIntervals.add(0, new TimeRange(TimeRange.START_OF_DAY, TimeRange.START_OF_DAY));
+    optionalBusyIntervals.add(new TimeRange(TimeRange.END_OF_DAY+1, TimeRange.END_OF_DAY+1));
     //this loop calculates size of free space between two subsequent intervals, and checks if size meets requirements
     //if so it adds this space to freeIntervals that stores possible place for requested event
     for(int i = 0; i < busyIntervals.size()-1; i++){
@@ -60,8 +96,15 @@ public final class FindMeetingQuery {
       if(duration > 0 && duration >= request.getDuration())
         freeIntervals.add(new TimeRange(busyIntervals.get(i).end(), busyIntervals.get(i+1).start()-busyIntervals.get(i).end()));
     }
+    for(int i = 0; i < optionalBusyIntervals.size()-1; i++){
+      duration = optionalBusyIntervals.get(i+1).start()-optionalBusyIntervals.get(i).end();
+      if(duration > 0 && duration >= request.getDuration())
+        freeIntervalsWithOptionals.add(new TimeRange(optionalBusyIntervals.get(i).end(), optionalBusyIntervals.get(i+1).start()-optionalBusyIntervals.get(i).end()));
+    }
 
-    return freeIntervals;
+    //
+    if(freeIntervalsWithOptionals.size() == 0) return freeIntervals;
+    else return freeIntervalsWithOptionals;
   }
 
   private TimeRange mergeTimeRanges(TimeRange a, TimeRange b){
